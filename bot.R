fae53b5da115ba20cfc38f784a593d0a42c53233
Jovan Tanevski <tanevski@gmail.com>
2020-06-03 10:54:10 +0200

    truncate posts with excessive length

--- bot.R
+++ bot.R
@@ -23,20 +23,29 @@ webhooks %>% pwalk(function(...) {
     blocks <- list.filter(refs, f1000AddedDate > lastDate) %>% map(function(r) {
       noteResp <- GET(
         paste0("https://sciwheel.com/extapi/work/references/", r$id, "/notes?"),
-        add_headers(Authorization = paste("Bearer", f1000auth)))
-      note <- ifelse(noteResp$status_code == 200 & length(content(noteResp)) > 0, 
-                     paste0(list.sort(content(noteResp), created)[[1]]$comment, "\n"), "")
-      
+        add_headers(Authorization = paste("Bearer", f1000auth))
+      )
+      note <- ifelse(noteResp$status_code == 200 & length(content(noteResp)) > 0,
+        paste0(list.sort(content(noteResp), created)[[1]]$comment, "\n"), ""
+      )
+
       details <- paste0(
         note,
         r$authorsText, ". <https://sciwheel.com/work/#/items/", r$id, "/detail?collection=", current$projectId, "|", r$title, "> ",
-        r$journalName, ". ", r$publishedYear, 
+        r$journalName, ". ", r$publishedYear,
         " - added by: ", r$f1000AddedBy,
         ifelse(length(r$f1000Tags) > 0,
           paste0(" - tags: ", paste0(r$f1000Tags, collapse = " ")),
           ""
         )
       )
+
+      if (nchar(details) > 3000) {
+        details <- paste0(
+          "Full information too long to display. <https://sciwheel.com/work/#/items/",
+          r$id, "/detail?collection=", current$projectId, "|", r$title, "> "
+        )
+      }
 
       list(type = "section", text = list(type = "mrkdwn", text = details))
     })
@@ -46,9 +55,9 @@ webhooks %>% pwalk(function(...) {
 
       content <- toJSON(list(blocks = blocks), auto_unbox = TRUE)
       outcome <- POST(url = webhook, content_type_json(), body = content)
-      
+
       print(paste(Sys.time(), "POST status", outcome$status_code))
-      
+
       if (outcome$status_code == 200) {
         if (refs[[1]]$f1000AddedDate > templastDate) {
           templastDate <<- refs[[1]]$f1000AddedDate
@@ -58,10 +67,7 @@ webhooks %>% pwalk(function(...) {
   }
 })
 
-if(templastDate > lastDate){
+if (templastDate > lastDate) {
   lastDate <- templastDate
   save(f1000auth, webhooks, lastDate, file = "state.rdata")
 }
-
-
-
