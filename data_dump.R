2afd0afaf46afe852154c24ce965c815165c0e6a
Jovan Tanevski <tanevski@gmail.com>
2021-06-04 14:07:33 +0200

    get content only for notes

--- data_dump.R
+++ data_dump.R
@@ -16,19 +16,19 @@ dump <- distinct.webhooks %>% pmap(function(...) {
     add_headers(Authorization = paste("Bearer", f1000auth))
   )
   print(paste(Sys.time(), "channel", current$channel, "GET status", resp$status_code))
-  
-  
+
+
   if (resp$status_code == 200) {
     project.content <- content(resp)
-    
+
     project.notes <- project.content$results %>% map(function(r) {
-      noteResp <- GET(
+      GET(
         paste0("https://sciwheel.com/extapi/work/references/", r$id, "/notes?"),
         add_headers(Authorization = paste("Bearer", f1000auth))
-      )
+      ) %>% content()
     })
-    
-    names(project.notes) <- project.content$results %>% map_chr(~.x$id)
+
+    names(project.notes) <- project.content$results %>% map_chr(~ .x$id)
     list(content = project.content, notes = project.notes)
   } else {
     NULL
@@ -37,7 +37,7 @@ dump <- distinct.webhooks %>% pmap(function(...) {
 
 names(dump) <- distinct.webhooks$channel
 
-if(file.exists("data_dump.rds")){
+if (file.exists("data_dump.rds")) {
   file.rename("data_dump.rds", "data_dump_old.rds")
 }
 
