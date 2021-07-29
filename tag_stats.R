d5f078800b931ce0ac337e859499f40fde30ba06
Jovan Tanevski <tanevski@gmail.com>
2021-07-29 13:49:56 +0200

    generate tag stats from new dump structure

--- tag_stats.R
+++ tag_stats.R
@@ -1,11 +1,12 @@
 library(dplyr)
 library(purrr)
 library(ggplot2)
+library(stringr)
 
 dump <- readRDS("data_dump.rds")
 
 tags <- unlist(dump %>%
-                 map(~ .x$content$results %>%
+                 map(~ .x$content %>%
                        map(~ .x$f1000Tags)))
 
 tag.stats <- tibble(channel = str_remove(names(tags),"\\d+"), tag = tags)
