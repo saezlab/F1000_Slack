3d2846b5fa88dff9da085e6a8b12a17266243d6d
Jovan Tanevski <tanevski@gmail.com>
2021-09-08 14:33:47 +0200

    partial match of mentions to display name

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ A Slack app companion script connecting F1000/Sciwheel to Slack
 ## Requirements
 The bot requires a `state.rdata` object
 
-This object should contain 2 variables:
+This object should contain 3 variables:
+
+- `bottoken` - *character*, a varialb containing the Slack authetincation token for the bot.
 
 - `f1000auth` - *character*, a variable containing the F1000/Sciwheel external API access token.
 - `webhooks` - *data.frame*, a table with four columns - (*character*) channel, (*numeric*) projectId, (*character*) webhook and (_numeric_) lastDate. Each row contains information about the mapping between the name of the slack channel, the F1000/Sciwheel subproject id from where the papers will be queried and posted to that channel, the complete url of the Slack webhook that the app can use to post to that channel and the last time  (in milliseconds since  Jan 1, 1970 00:00:00 UTC) F1000/Sciwheel  was queried for that channel/projectId.
--- bot.R
+++ bot.R
@@ -5,44 +5,74 @@ suppressPackageStartupMessages({
   library(dplyr)
   library(stringr)
   library(rlist)
+  library(slackr)
 })
 
 load("state.rdata")
 
+slackr_setup(token = bottoken)
+users <- slackr_users()
+
 # returns last date
-lastDates <- webhooks %>% pmap_dbl(function(...) {
+lastDates <- webhooks %>% pmap_dbl(\(...) {
   current <- data.frame(...)
 
   templastDate <- current$lastDate
-  
+
   resp <- GET(
-    paste0("https://sciwheel.com/extapi/work/references?projectId=", current$projectId, "&sort=addedDate:desc"),
+    paste0(
+      "https://sciwheel.com/extapi/work/references?projectId=",
+      current$projectId, "&sort=addedDate:desc"
+    ),
     add_headers(Authorization = paste("Bearer", f1000auth))
   )
-  print(paste(Sys.time(), "channel", current$channel, "GET status", resp$status_code))
+  print(paste(
+    Sys.time(), "channel", current$channel, "GET status",
+    resp$status_code
+  ))
 
   if (resp$status_code == 200) {
-    refs <- content(resp)$results %>% 
-      list.filter(f1000AddedDate > templastDate) %>% 
+    refs <- content(resp)$results %>%
+      list.filter(f1000AddedDate > templastDate) %>%
       rev()
-    
-    blocks <- refs  %>% map(function(r) {
+
+    blocks <- refs %>% map(\(r) {
       noteResp <- GET(
         paste0("https://sciwheel.com/extapi/work/references/", r$id, "/notes?"),
         add_headers(Authorization = paste("Bearer", f1000auth))
       )
-    
-      
-      # references to usernames will be matched and converted to lowercase per agreement
+
       comments <- list.filter(content(noteResp), is.null(highlightText))
+
       note <- ifelse(noteResp$status_code == 200 & length(comments) > 0,
-                     paste0(list.sort(comments, created)[[1]]$comment, "\n"), ""
-      ) %>% str_replace_all("@\\w+", tolower)
-      
+        paste0(list.sort(comments, created)[[1]]$comment, "\n"), ""
+      ) %>% str_replace_all("@\\w+", \(name){
+        match <- str_which(
+          users %>%
+            pull(display_name_normalized) %>%
+            str_remove_all("\\s") %>%
+            tolower(),
+          str_remove(name, "@") %>% tolower()
+        )
+
+        if (length(match) == 9) {
+          return(name)
+        }
+
+        users %>%
+          slice(match) %>%
+          mutate(
+            ref = paste0("<@", id, ">"),
+            match.per = (nchar(!!name) - 1) / nchar(display_name_normalized)
+          ) %>%
+          arrange(-match.per) %>%
+          pluck("ref", 1)
+      })
 
       details <- paste0(
         note,
-        r$authorsText, ". <https://sciwheel.com/work/#/items/", r$id, "/detail?collection=", current$projectId, "|", r$title, "> ",
+        r$authorsText, ". <https://sciwheel.com/work/#/items/",
+        r$id, "/detail?collection=", current$projectId, "|", r$title, "> ",
         r$journalName, ". ", r$publishedYear,
         " - added by: ", r$f1000AddedBy,
         ifelse(length(r$f1000Tags) > 0,
@@ -53,45 +83,48 @@ lastDates <- webhooks %>% pmap_dbl(function(...) {
 
       if (nchar(details) > 3000) {
         details <- paste0(
-          "Full information too long to display. <https://sciwheel.com/work/#/items/",
+          "Full information too long to display. ",
+          "<https://sciwheel.com/work/#/items/",
           r$id, "/detail?collection=", current$projectId, "|", r$title, "> "
         )
       }
-      
+
       list(type = "section", text = list(type = "mrkdwn", text = details))
     })
 
     if (length(blocks) > 0) {
       webhook <- as.character(current$webhook)
-      
+
       # one paper at a time with wait in between so we don't get rate limited
       alive <- TRUE
-      respcodes <- blocks %>% map2_int(refs %>% map(~.x$f1000AddedDate), function(block, addedDate){
-        if(alive){
-          Sys.sleep(1.05)
-          content <- toJSON(list(blocks = list(block)), auto_unbox = TRUE)
-          outcome <- POST(url = webhook, content_type_json(), body = content)
-          if(outcome$status_code == 200){
-            if (addedDate > templastDate) {
-              templastDate <<- addedDate
+      respcodes <- blocks %>% map2_int(
+        refs %>% map(~ .x$f1000AddedDate),
+        \(block, addedDate) {
+          if (alive) {
+            Sys.sleep(1.05)
+            content <- toJSON(list(blocks = list(block)), auto_unbox = TRUE)
+            outcome <- POST(url = webhook, content_type_json(), body = content)
+            if (outcome$status_code == 200) {
+              if (addedDate > templastDate) {
+                templastDate <<- addedDate
+              }
+            } else {
+              alive <<- FALSE
             }
+            outcome$status_code
           } else {
-            alive <<- FALSE
+            -1
           }
-          outcome$status_code
-        } else {
-          -1
         }
-      })
+      )
 
       print(paste(Sys.time(), "POST status", paste(respcodes, collapse = ", ")))
-      
     }
   }
   templastDate
 })
 
-if(sum(lastDates > webhooks$lastDate) > 0){
+if (sum(lastDates > webhooks$lastDate) > 0) {
   webhooks$lastDate <- lastDates
-  save(f1000auth, webhooks, file = "state.rdata")
+  save(bottoken, f1000auth, webhooks, file = "state.rdata")
 }
